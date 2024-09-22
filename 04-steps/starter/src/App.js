6ba79eb294f573a7494aa692bd57c9b875eeb70c
Ramsnes <115258511+Ramsnes@users.noreply.github.com>
2024-09-22 19:15:55 +0200

    added children code to 04-steps

--- 04-steps/starter/src/App.js
+++ 04-steps/starter/src/App.js
@@ -7,6 +7,10 @@ const messages = [
 ];
 
 export default function App() {
+  return <Steps />;
+}
+
+function Steps() {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
 
@@ -39,21 +43,28 @@ export default function App() {
           </p>
 
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
+            <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrevious}>
+              <span>👈 Previous</span>
+            </Button>
+
+            <Button textColor="#fff" bgColor="#7950f2" onClick={handleNext}>
+              <span>👉 Next</span>
+            </Button>
           </div>
         </div>
       )}
     </>
+  );
+}
+
+// {children} prop is a predetermined prop in React that let's ut pass any jsx into a non-closing componen and fill that hole
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
